4ba1e45ea7a1034f9dd42edc7d23addf532f5e7e
prithvikings <prithvi07raj07@gmail.com>
2024-11-03 03:00:11 +0530

    completed basic weather app project

---
+++ script.js
@@ -0,0 +1,59 @@
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.getElementById('searchBtn');
+const weather_Img = document.querySelector('.weather-img');
+const temperature = document.querySelector('.temprature');
+const description = document.querySelector('.description');
+const humidity = document.getElementById('humidity'); 
+const wind_speed = document.getElementById('wind-speed'); 
+const location_not_found = document.querySelector('.location-not-found');
+const weather_body = document.querySelector('.weather-body');
+
+const checkweather=async (city)=>{
+    const api="c6bc976394f13845dc637ae1be0ff583";
+    const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`;
+    const weather_data=await fetch(url);
+    try{
+        const data=await weather_data.json();
+        console.log(data);
+        if(data.cod=="404"){
+            console.error("City not found");
+            location_not_found.style.display="flex";
+            weather_body.style.display="none";
+            return;
+        }
+        location_not_found.style.display="none"; 
+        weather_body.style.display="flex";
+        temperature.innerHTML=`${Math.round(data.main.temp-273.15)}°C`;
+        description.innerHTML=`${data.weather[0].description}`;
+        humidity.innerHTML=`${data.main.humidity}%`;
+        wind_speed.innerHTML=`${data.wind.speed} Km/H`;
+
+        const weather=data.weather[0].main;
+        if(weather=="Clouds"){
+            weather_Img.src="./assets/cloud.png";
+        }else if(weather=="Rain"){
+            weather_Img.src="./assets/rain.png";
+        }
+        else if(weather=="Clear"){
+            weather_Img.src="./assets/clear.png";
+        }else if(weather=="Haze"){
+            weather_Img.src="./assets/mist.png";
+        }else if(weather=="Mist"){
+            weather_Img.src="./assets/mist.png";
+        }
+        else if(weather=="Snow"){
+            weather_Img.src="./assets/snow.png";
+        }
+        else if(weather=="Thunderstorm"){
+            weather_Img.src="./assets/rain.png";
+        }
+    }catch(e){
+        console.error("City not found");
+    }
+}
+
+searchBtn.addEventListener('click', () => {
+    checkweather(inputBox.value);
+});
+
+
